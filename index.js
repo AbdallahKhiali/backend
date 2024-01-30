6b3eef48d7718f4d01d9e2162df1ea51ab598486
AbdallahKhiali <work.khiali@gmail.com>
2024-01-30 14:44:44 +0100

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const Connection = require('./config/db.js');
+const Connection = require('./config/db.js');
 var cors = require('cors');
 var mongoose = require("mongoose");
 
@@ -9,20 +9,19 @@ var mongoose = require("mongoose");
 const userRouter = require('./routers/user.js');
 const postRouter = require('./routers/post.js');
 const commentRouter = require('./routers/comment.js');
-var configData = require("./config/connection");
 
 
 require('dotenv').config()
-
-// Database
-var connectionInfo = configData.getConnectionInfo();
-mongoose.connect(connectionInfo.DATABASE_URL);
 
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
